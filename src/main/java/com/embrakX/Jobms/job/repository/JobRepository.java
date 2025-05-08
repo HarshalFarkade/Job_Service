46a67a94493ab5f74cf29cafc9c4ca801686adbd
Harshal <farkadeharshal14@gmail.com>
2025-05-08 14:38:55 +0530

    done with development

---
+++ src/main/java/com/embrakX/Jobms/job/repository/JobRepository.java
@@ -0,0 +1,9 @@
+package com.embrakX.jobms.job.repository;
+
+import com.embrakX.jobms.job.entity.Job;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface JobRepository extends JpaRepository<Job,Long> {
+}
